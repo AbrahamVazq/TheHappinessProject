1a071e177cdce59e10e4c8864077f6bd5a554b52
Abraham Vazquez <mabraham.vazquez@gmail.com>
2024-04-13 20:35:16 -0600

    Initial Commit

---
+++ TheHappinessProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheHappinessProject
+//
+//  Created by Abraham Vazquez on 13/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
